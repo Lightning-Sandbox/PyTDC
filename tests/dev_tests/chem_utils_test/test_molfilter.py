506460181dd0e8c893d5e3bc7d60f85032f73778
Jirka B <j.borovec+github@gmail.com>
2025-07-02 11:29:04 +0200

    fix

--- tests/dev_tests/chem_utils_test/test_molfilter.py
+++ tests/dev_tests/chem_utils_test/test_molfilter.py
@@ -6,6 +6,8 @@ from __future__ import print_function
 from tdc.chem_utils import MolFilter
 
 
-def test_MolConvert(self):
-    filters = MolFilter(filters=["PAINS"], HBD=[0, 6])
-    filters(["CCSc1ccccc1C(=O)Nc1onc2c1CCC2"])
+class TestMolFilter:
+
+    def test_MolConvert(self):
+        filters = MolFilter(filters=["PAINS"], HBD=[0, 6])
+        filters(["CCSc1ccccc1C(=O)Nc1onc2c1CCC2"])
